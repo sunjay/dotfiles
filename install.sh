6e64d3cba243a2d7c55aa10b6cd7d0ecb0490790
Sunjay Varma <varma.sunjay@gmail.com>
2015-10-09 15:48:16 -0400

    Finished basic install script

--- install.sh
+++ install.sh
@@ -1,23 +1,39 @@
 ##########################################################
+# 
 # Basic Installation Script
 # * Links dotfiles into their corresponding locations
 #   * home/ -> ~ or $HOME
+# 
 ##########################################################
 
 symlinkDotfiles() {
-    if [-z "$1"] then
+    if [ -z "$1" ]; then
         echo "Usage: symlinkDotfiles(from, to)"
         exit -1
     fi
 
-    if [-z "$2"] then
+    if [ -z "$2" ]; then
         echo "Usage: symlinkDotfiles(from, to)"
         exit -1
     fi
 
+    from=$(readlink -f $1)
+    to=$(readlink -f $2)
+
+    echo "Copying dotfiles from $from to $to"
+    echo
+
+    files=$(ls $from)
+    for file in $files; do
+        echo "Backing up any existing file with a .old extension"
+        mv $to/.$file $to/.$file.old
+
+        echo "Creating symlink to $from/$file in $to/.$file"
+        ln -s $from/$file $to/.$file
+    done
 }
 
 # Symlink home/ files
-
+symlinkDotfiles "home" ${HOME-~}
 
 
